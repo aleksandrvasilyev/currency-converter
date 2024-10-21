6aa1fc2f9258a4f6e293cb33ed5619ed56ebcb02
Alexander Vasyliev <saddon11@gmail.com>
2024-10-22 00:37:09 +0200

    add language storage, redirect and view layout

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import i18nextMiddleware from "i18next-http-middleware";
 import Backend from "i18next-fs-backend";
 import path from "path";
 import { fileURLToPath } from "url";
+import expressLayouts from "express-ejs-layouts";
 
 const app = express();
 
@@ -14,10 +15,19 @@ const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 
+app.use(expressLayouts);
+
 app.set("view engine", "ejs");
+app.set("layout", "./layouts/main");
 app.set("views", "views");
 
 app.use(express.static("public"));
+
+const languages = {
+  en: "🇬🇧 English",
+  ru: "🇷🇺 Русский",
+  de: "🇩🇪 Deutsch",
+};
 
 await i18next
   .use(Backend)
@@ -33,33 +43,52 @@ await i18next
   });
 
 app.use(i18nextMiddleware.handle(i18next));
+
 const localizationMiddleware = (req, res, next) => {
   const supportedLangs = ["ru", "de", "en"];
   const urlParts = req.url.split("/");
+  // console.log(urlParts);
 
   if (supportedLangs.includes(urlParts[1])) {
     req.language = urlParts[1];
+    // console.log(urlParts);
+
     req.url = "/" + urlParts.slice(2).join("/");
+
+    // urlParts.splice(1, 1);
+    // req.url = urlParts.join("/");
+
+    // console.log(req.url);
   } else {
     req.language = "en";
   }
 
   req.i18n.changeLanguage(req.language);
 
-  res.locals.urlFor = (path, lang) => {
-    let cleanPath = path.replace(/^\/(?:ru|de|en)/, "");
-
-    if (cleanPath === "") cleanPath = "/";
-
-    if (lang && lang !== "en") {
-      return `/${lang}${cleanPath}`;
+  res.locals.changeLangLink = (lang) => {
+    // console.log(req.url);
+    // console.log(req.route.path);
+    if (lang !== "en") {
+      return "/" + lang + req.url;
     } else {
-      return cleanPath;
+      return req.url;
     }
   };
 
+  res.locals.link = (path) => {
+    if (req.language !== "en") {
+      return "/" + req.language + path;
+    } else {
+      return path;
+    }
+  };
+
+  res.locals.lang = languages[req.language];
+
+  res.locals.language = req.language;
   next();
 };
+
 app.use(localizationMiddleware);
 
 app.get("/", async (req, res) => {
@@ -68,18 +97,27 @@ app.get("/", async (req, res) => {
   const { result: data, date: date } = await getCurrencies(url);
   const emojis = emojisContainer();
 
-  res.render("index", { data, date, emojis, t: req.t, req: req });
+  res.render("index", {
+    data,
+    date,
+    emojis,
+    t: req.t,
+    req: req,
+  });
 });
 
 app.get("/about", (req, res) => {
-  // res.render("about", {
-  //   t: req.t,
-  //   req: req,
-  // });
+  res.render("about", {
+    t: req.t,
+    req: req,
+  });
+});
 
-  const date = "2024"; // hardcoded -> remove or change
-
-  res.render("about", { date, t: req.t, req: req });
+app.get("/about/:id/:aq", (req, res) => {
+  res.render("about", {
+    t: req.t,
+    req: req,
+  });
 });
 
 app.use((req, res) => {
--- public/script.js
+++ public/script.js
@@ -1,58 +1,112 @@
 const inputs = document.querySelectorAll("input");
+const currency = () => {
+  const getCurrency = (event, input) => {
+    const isDigit = /^\d+$/.test(event.key);
+    const isBackspace = event.key === "Backspace";
+    const isDelete = event.key === "Delete";
 
-const getCurrency = (event, input) => {
-  const isDigit = /^\d+$/.test(event.key);
-  const isBackspace = event.key === "Backspace";
-  const isDelete = event.key === "Delete";
-
-  if (input.value) {
-    if (isDigit || isBackspace || isDelete) {
-      reCount(input, input.value);
+    if (input.value) {
+      if (isDigit || isBackspace || isDelete) {
+        reCount(input, input.value);
+      }
     }
-  }
 
-  if (input.value === "") {
-    clearAllInputs();
-  }
-};
+    if (input.value === "") {
+      clearAllInputs();
+    }
+  };
 
-const clearAllInputs = () => {
+  const clearAllInputs = () => {
+    inputs.forEach((input) => {
+      input.value = "";
+    });
+  };
+
+  const reCount = (input, value) => {
+    const currencies = Array.from(inputs).filter(
+      (item) => item.id !== input.id
+    );
+
+    currencies.forEach((element) => {
+      if (input.id === "usd") {
+        element.value = Number(value * element.dataset.value).toFixed(2);
+      } else {
+        element.value = Number(
+          (1 / input.dataset.value) * element.dataset.value * value
+        ).toFixed(2);
+      }
+    });
+  };
+
   inputs.forEach((input) => {
-    input.value = "";
+    input.addEventListener("keyup", (event) => {
+      getCurrency(event, input);
+    });
   });
 };
 
-const reCount = (input, value) => {
-  const currencies = Array.from(inputs).filter((item) => item.id !== input.id);
+const dropdownLanguages = () => {
+  const dropdownButton = document.getElementById("dropdownButton");
+  const dropdownMenu = document.getElementById("dropdownMenu");
+  const linksLanguage = document.querySelectorAll("#dropdownMenu li");
 
-  currencies.forEach((element) => {
-    if (input.id === "usd") {
-      element.value = Number(value * element.dataset.value).toFixed(2);
-    } else {
-      element.value = Number(
-        (1 / input.dataset.value) * element.dataset.value * value
-      ).toFixed(2);
+  dropdownButton.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("hidden");
+  });
+
+  window.addEventListener("click", function (e) {
+    if (
+      !dropdownButton.contains(e.target) &&
+      !dropdownMenu.contains(e.target)
+    ) {
+      dropdownMenu.classList.add("hidden");
     }
   });
+
+  linksLanguage.forEach((li) => {
+    li.addEventListener("click", (e) => {
+      const link = li.querySelector("a");
+      if (link) {
+        const selectedLang = link.getAttribute("data-lang");
+        console.log(selectedLang);
+        localStorage.setItem("selectedLanguage", selectedLang);
+      }
+    });
+  });
 };
 
-inputs.forEach((input) => {
-  input.addEventListener("keyup", (event) => {
-    getCurrency(event, input);
-  });
-});
+const redirectToLanguage = () => {
+  const supportedLangs = ["ru", "de", "en"];
+  const url = window.location.pathname;
+  const storageLanguage = localStorage.getItem("selectedLanguage");
+  const urlLanguage = document.querySelector("html").lang;
 
-const dropdownButton = document.getElementById("dropdownButton");
-const dropdownMenu = document.getElementById("dropdownMenu");
+  if (urlLanguage !== storageLanguage) {
+    const urls = url.split("/");
+    if (!supportedLangs.includes(urls[1])) {
+      window.location.replace("/" + storageLanguage + url);
+    } else {
+      // urls.splice(1, 1);
+      // window.location.replace("/" + storageLanguage + urls.join("/"));
 
-// Добавляем обработчик события для клика по кнопке
-dropdownButton.addEventListener("click", () => {
-  dropdownMenu.classList.toggle("hidden");
-});
+      // window.location.replace(
+      //   "/" + storageLanguage + "/" + urls.slice(2).join("/")
+      // );
 
-// Закрытие dropdown, если клик произошел вне меню
-window.addEventListener("click", function (e) {
-  if (!dropdownButton.contains(e.target) && !dropdownMenu.contains(e.target)) {
-    dropdownMenu.classList.add("hidden");
+      if (storageLanguage === "en") {
+        window.location.href = "/" + urls.slice(2).join("/");
+      } else {
+        window.location.href =
+          "/" + storageLanguage + "/" + urls.slice(2).join("/");
+      }
+    }
   }
-});
+};
+
+const main = () => {
+  currency();
+  dropdownLanguages();
+  redirectToLanguage();
+};
+
+main();
